9cadc69dd229bc84f9f1633e0f321d570e8fe95e
Caleb <caleb.t.coons@gmail.com>
2025-07-14 17:33:59 -0600

    I have been trying to figure out how all the pieces fit together. I have made basic outlines of several classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
+import dataaccess.*;
 import spark.*;
 
 public class Server {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,6 +15,10 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        this.authDAO = new MemoryAuthDAO();
+        this.gameDAO = new MemoryGameDAO();
+        this.userDAO = new MemoryUserDAO();
+
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
