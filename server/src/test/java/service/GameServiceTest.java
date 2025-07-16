e22ff31a21a2db88807c6d8491a7b4fff3b37227
Caleb <caleb.t.coons@gmail.com>
2025-07-16 17:58:52 -0600

    set up my Game Service unit tests

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,31 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GameServiceTest {
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void getGameByName() {
+    }
+
+    @Test
+    void getGameByID() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void clearGameData() {
+    }
+}
