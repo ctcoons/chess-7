c3b37f94a8a202499c640525ccd5c7e6998dac8e
Caleb <caleb.t.coons@gmail.com>
2025-07-16 17:57:35 -0600

    Wrote the first of my unit tests for AuthServices

---
+++ server/src/test/java/service/AuthServiceTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Test
+    void addAuth() {
+    }
+
+    @Test
+    void getAuthByUsername() {
+    }
+
+    @Test
+    void getUsernameByAuthToken() {
+    }
+
+    @Test
+    void deleteAuth() {
+    }
+
+    @Test
+    void validateAuth() {
+    }
+
+    @Test
+    void clearAuthData() {
+    }
+}
